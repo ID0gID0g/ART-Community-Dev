cfc9fa811adef6105b4a52a379ec58eda8605083
IDogIDog <106388567+ID0gID0g@users.noreply.github.com>
2023-05-01 15:19:12 +0300

    added a framework for the Database and some other changes

---
+++ src/main/java/tools/Collection.java
@@ -0,0 +1,7 @@
+package tools;
+
+public class Collection {
+    public static final String collection_arts = "Arts";
+    public static final String collection_users = "Users";
+    public static final String collection_statistics = "Statistics";
+}
--- src/main/java/tools/DataBase.java
+++ src/main/java/tools/DataBase.java
@@ -1,7 +1,38 @@
 package tools;
 
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import java.util.Objects;
+
 public class DataBase {
     public static void main(String[] args) {
+        find(Collection.collection_arts, "id","123");
+        delete(Collection.collection_arts, "id", "456");
+    }
+    private static final MongoDatabase database = MongoClients.create(
+            Objects.requireNonNull(Config.getConfig("data_base_url")))
+            .getDatabase(Objects.requireNonNull(Config.getConfig("data_base_name"))
+            );
 
+    public static void find(String collectionType, String heading, String value){
+        Document searchQuery = new Document();
+        searchQuery.put(heading, value);
+        FindIterable<Document> cursor = database.getCollection(collectionType).find(searchQuery);
+
+        try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+            while (cursorIterator.hasNext()) {
+                System.out.println(cursorIterator.next());
+            }
+        }
+    }
+    public static void add(String collectionType, Document document){
+        database.getCollection(collectionType).insertOne(document);
+    }
+    public static void delete(String collectionType, String heading, String value){
+        Document searchQuery = new Document();
+        searchQuery.put(heading, value);
+
+        database.getCollection(collectionType).deleteOne(searchQuery);
     }
 }
